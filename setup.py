7361cd7f32444db9cb28a701206a11ed673c2ac0
Marc Vitalis <marc.vitalis@live.com>
2021-08-03 17:18:11 +0800

    fix build

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='StringiPy',
-      version='0.1',
+      version='0.1.0',
       description='Python Functional String Utilities',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Marc Vitalis',
       author_email='marc.vitalis@live.com',
       url='https://github.com/netxph/stringipy',
-      packages=['StringiPy'])
+      packages=['stringipy'])
